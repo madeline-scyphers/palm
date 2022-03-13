7e9d41af454247a6582313d6491e18e48046c7bb
Madeline Scyphers <madelinescyphers@gmail.com>
2022-03-12 19:54:58 -0500

    add some changes to config template

--- data_analyzer/analysis_utils.py
+++ data_analyzer/analysis_utils.py
@@ -2,8 +2,8 @@ import xarray as xr
 
 def load_data_set(data_path):
     ds_3d = xr.open_dataset(data_path, chunks="auto")
-    ds_3d = ds_3d.drop_isel(zu_3d=0, zw_3d=0)  # nan value
-    ds_3d = ds_3d.reset_index(["zu_3d", "zw_3d"])
+    # ds_3d = ds_3d.drop_isel(zu_3d=0, zw_3d=0)  # nan value
+    # ds_3d = ds_3d.reset_index(["zu_3d", "zw_3d"])
     return ds_3d
 
 
