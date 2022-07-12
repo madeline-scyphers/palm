7131c62ce10da575e8ff91502258b16cf2eb6f18
Madeline Scyphers <madelinescyphers@gmail.com>
2022-07-12 19:51:16 -0400

    Add run file for optimization

--- palm_wrapper/optimize/wrapper.py
+++ palm_wrapper/optimize/wrapper.py
@@ -1,5 +1,6 @@
 import copy
 import datetime as dt
+import os
 import subprocess
 import json
 from pathlib import Path
@@ -24,60 +25,68 @@ JOB_SCRIPT_PATH = Path(__file__).resolve().parent / "batch_job_template.txt"
 
 
 class Wrapper(BaseWrapper):
-    def __init__(self, config):
-        self.config = config
+    def __init__(self):
+        self.config = None
+        self.model_settings = None
+        self.ex_settings = None
+        self.experiment_dir = None
 
+    def load_config(self, config_file: os.PathLike):
+        config = load_yaml(config_file)
+        experiment_name = get_dt_now_as_str()
+        config["optimization_options"]["experiment"]["name"] = experiment_name
+
+        self.config = config
         self.model_settings = self.config["model_options"]
         self.ex_settings = self.config["optimization_options"]
-
-        self.wrapper_config = None
+        self.experiment_dir = Path(self.model_settings["optimization_output_dir"]).expanduser() / experiment_name
 
     def write_configs(self, trial: Trial) -> None:
-        wrapper_config = copy.deepcopy(self.config)
+        trial_config = copy.deepcopy(self.config)
         job_name = zfilled_trial_index(trial.index)
-        optimization_output_dir = Path(self.model_settings["optimization_output_dir"]).expanduser()
-        experiment_name = self.ex_settings["experiment"]["name"]
-        job_output_dir = optimization_output_dir / experiment_name / job_name
+        job_output_dir = self.experiment_dir / job_name
         job_output_dir.mkdir(parents=True)
-
-        log_file = job_output_dir / f"{job_name}_%j.log"
-        run_time = wrapper_config["model_options"]["output_end_time"] * wrapper_config["model_options"]["palmrun_walltime_scalar"]
-        data_analyses_time = (
-                (wrapper_config["model_options"]["output_end_time"] - wrapper_config["model_options"]["output_start_time"])
-                * wrapper_config["model_options"]["data_analyse_walltime_scalar"])
-        batch_time = run_time + data_analyses_time
-        io_config = wrapper_config["model_options"]["io_config"]
-        wrapper_config_path = job_output_dir / "wrapper_config.yaml"
-        job_script_path = (job_output_dir / "slurm_job.sh").resolve()
-
-        wrapper_config["parameters"] = trial.arm.parameters
-        wrapper_config["model_options"]["config_path"] = wrapper_config_path
-        wrapper_config["model_options"]["job_script_path"] = job_script_path
-        wrapper_config["model_options"]["job_name"] = job_name
-        wrapper_config["model_options"]["log_file"] = log_file
-        wrapper_config["model_options"]["job_output_dir"] = job_output_dir
-        wrapper_config["model_options"]["run_time"] = run_time
-        wrapper_config["model_options"]["data_analyses_time"] = data_analyses_time
-        wrapper_config["model_options"]["batch_time"] = batch_time
-        wrapper_config["model_options"]["io_config"] = io_config
+        job_script_path, trial_config_path = self._update_trial_config(trial_config, trial, job_name, job_output_dir)
+        trial.update_run_metadata(
+            dict(trial_config_path=trial_config_path,
+                 job_script_path=job_script_path))
 
         with open(JOB_SCRIPT_PATH) as template:
             job_script = template.read()
-        job_script.format(**wrapper_config["model_options"])
+        job_script.format(**trial_config["model_options"])
 
         with open(job_script_path, "w") as f:
             f.write(job_script)
 
-        with open(wrapper_config_path, 'w') as f:
-            yaml.dump(wrapper_config, f)
-        trial.update_run_metadata(
-            dict(wrapper_config_path=wrapper_config_path,
-                 job_script_path=job_script_path))
+        with open(trial_config_path, 'w') as f:
+            yaml.dump(trial_config, f)
+
+    @staticmethod
+    def _update_trial_config(trial_config, trial, job_name, job_output_dir):
+        run_time = trial_config["model_options"]["output_end_time"] * trial_config["model_options"][
+            "palmrun_walltime_scalar"]
+        data_analyses_time = (
+                (trial_config["model_options"]["output_end_time"] - trial_config["model_options"][
+                    "output_start_time"])
+                * trial_config["model_options"]["data_analyse_walltime_scalar"])
+        trial_config_path = job_output_dir / "trial_config.yaml"
+        job_script_path = (job_output_dir / "slurm_job.sh").resolve()
+
+        trial_config["parameters"] = trial.arm.parameters
+        trial_config["model_options"]["config_path"] = trial_config_path
+        trial_config["model_options"]["job_script_path"] = job_script_path
+        trial_config["model_options"]["job_name"] = job_name
+        trial_config["model_options"]["log_file"] = job_output_dir / f"{job_name}_%j.log"
+        trial_config["model_options"]["job_output_dir"] = job_output_dir
+        trial_config["model_options"]["run_time"] = run_time
+        trial_config["model_options"]["data_analyses_time"] = data_analyses_time
+        trial_config["model_options"]["batch_time"] = run_time + data_analyses_time
+        return job_script_path, trial_config_path
 
     def run_model(self, trial: Trial):
-        wrapper_config = self._load_wrapper_config(trial)
+        trial_config = self._load_trial_config(trial)
 
-        job_script_path = wrapper_config["model_options"]["job_script_path"]
+        job_script_path = trial_config["model_options"]["job_script_path"]
         cmd = f"sbatch {job_script_path}"
 
         args = cmd.split()
@@ -89,9 +98,9 @@ class Wrapper(BaseWrapper):
         """Get status of the job by a given ID. For simplicity of the example,
         return an Ax `TrialStatus`.
         """
-        wrapper_config = self._load_wrapper_config(trial)
+        trial_config = self._load_trial_config(trial)
 
-        log_file = wrapper_config["model_options"]["log_file"]
+        log_file = trial_config["model_options"]["log_file"]
 
         if log_file.exists():
             with open(log_file, "r") as f:
@@ -104,8 +113,8 @@ class Wrapper(BaseWrapper):
                 trial.mark_completed()
 
     def fetch_trial_data(self, trial: Trial, *args, **kwargs):
-        wrapper_config = trial.run_metadata["wrapper_config_path"]
-        job_output_dir = wrapper_config["model_options"]["job_output_dir"]
+        trial_config = trial.run_metadata["trial_config_path"]
+        job_output_dir = trial_config["model_options"]["job_output_dir"]
         data_filepath = job_output_dir / "r_ca.json"
 
         with open(data_filepath, 'r') as f:
@@ -114,7 +123,7 @@ class Wrapper(BaseWrapper):
         return dict(a=r_ca)
 
     @staticmethod
-    def _load_wrapper_config(trial: Trial):
-        wrapper_config_path = trial.run_metadata["wrapper_config_path"]
-        wrapper_config = load_yaml(wrapper_config_path, normalize=False)
-        return wrapper_config
+    def _load_trial_config(trial: Trial):
+        trial_config_path = trial.run_metadata["trial_config_path"]
+        trial_config = load_yaml(trial_config_path, normalize=False)
+        return trial_config
