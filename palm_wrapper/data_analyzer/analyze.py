6312ffb4d283c2db6ed1601a8e75e5e1d1534579
Madeline Scyphers <madelinescyphers@gmail.com>
2022-07-22 20:45:40 -0400

    Fix the calculations with r_ca by updating how it calculates deposition, lad, it does the grid adjust for palm's weird grid, and other fixes

--- palm_wrapper/data_analyzer/analyze.py
+++ palm_wrapper/data_analyzer/analyze.py
@@ -41,28 +41,35 @@ def analyze_data(input_dir, output_dir, job_name):
 
     dz = config["domain"]["dz"]
 
-    data_3d_path = sorted(output_dir.glob("*3d*.nc"))[-1]
-    data_pr_path = sorted(output_dir.glob("*pr*.nc"))[-1]
+    data_3d_path = sorted(output_dir.glob("*3d*.nc"))[0]
+    data_pr_path = sorted(output_dir.glob("*pr*.nc"))[0]
     ds_3d = load_data_set(data_3d_path)
     ds_pr = load_data_set(data_pr_path)
     ds_3d = ds_3d.rename({"zu_3d": "z"})
     ds_lad = load_data_set(lad_path)
     ds_lad = ds_lad.rename({"zlad": "z"})
-    ds_3d = ds_3d.drop_isel(z=[0, 1, -1], x=-1, y=-1)
+    ds_3d = ds_3d.drop_isel(z=0, zw_3d=0)
 
-    urban_ratio = 0.5
+    scalar_exchange_coeff = 1
+
+    ds_3d["xu"] = ds_3d.x
+    ds_3d["yv"] = ds_3d.y
+    ds_3d["zw_3d"] = ds_3d.z
+
+    ds_3d = ds_3d.interp(xu=ds_3d.x, yv=ds_3d.y, zw_3d=ds_3d.z)
+
+    urban_ratio = config["domain"]["urban_ratio"]
     y_domain = np.arange(int(ds_3d.s.y.size * urban_ratio), ds_3d.s.y.size)
 
     z_scalar = np.arange(150 // dz, 300 // dz)
 
-    ds_3d = ds_3d.interp(xu=ds_3d.x, yv=ds_3d.y, zw_3d=ds_3d.z)
     tot = ds_3d.uu + ds_3d.vv + ds_3d.ww
     ubar = tot ** (1 / 2)
 
-    DR = ubar.isel(y=y_domain) * ds_3d.s.isel(y=y_domain)
+    DR = scalar_exchange_coeff * ubar.isel(y=y_domain) * ds_3d.s.isel(y=y_domain) * ds_lad.isel(y=y_domain).lad
 
     lai_vals = ds_lad.lad.sum(dim="z").values
-    lai = lai_vals[lai_vals != 0].mean()
+    lai = lai_vals.mean() * dz
 
     ubar_z_scalar = ubar.isel(y=y_domain, z=z_scalar).mean(skipna=True)
     scalar_gradient = ds_3d.isel(y=y_domain, z=z_scalar).s.mean(skipna=True)
@@ -72,16 +79,14 @@ def analyze_data(input_dir, output_dir, job_name):
     for mask in masks:
 
         ds_pr = ds_pr.rename({f"zwv_{mask}": f"z_{mask}"})
+        ds_pr = ds_pr.drop_isel({f"z_{mask}": 0})
         ds_pr = ds_pr.interp({f"zwu_{mask}": ds_pr[f"z_{mask}"]})
 
-        ustar = (ds_pr[f"wu_{mask}"]**2 + ds_pr[f"wv_{mask}"]**2) ** (1 / 4)
+        ustar = (ds_pr[f"wu_{mask}"] ** 2 + ds_pr[f"wv_{mask}"] ** 2) ** (1 / 4)
+        ustar_bar = ustar.isel({f"z_{mask}": z_scalar}).mean()
 
-        ustar_above_canopy = ustar[f"z_{mask}"] > ds_lad.z.max()
-        first_ustar_above = ustar[f"z_{mask}"][ustar_above_canopy][0]
-        ustar_bar = ustar.sel({f"z_{mask}": first_ustar_above}).mean()
-
-        r_a = ubar_z_scalar / (ustar_bar**2) + 6.2 / (ustar_bar ** (2 / 3))
-        Depos = DR.mean(skipna=True) * ds_lad.lad.mean(skipna=True)
+        r_a = ubar_z_scalar / (ustar_bar ** 2) + 6.2 / (ustar_bar ** (2 / 3))
+        Depos = DR.mean(skipna=True)
 
         r_ca = ustar_bar * lai * (scalar_gradient / Depos - r_a - 1 / (ustar_bar * lai)).compute()
         r_cas.append(r_ca.values)
